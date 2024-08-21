e72d9f72326cacceede144747b86904c12e0c2a7
Carolina Leguizamon <63667103+ispeakfishlanguage@users.noreply.github.com>
2024-08-21 17:09:13 +0200

    refactor: update version to v1 in page_ml_performance.py

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -6,7 +6,7 @@ from src.machine_learning.evaluate_clf import load_test_evaluation
 
 
 def page_ml_performance_metrics():
-    version = 'v4'
+    version = 'v1'
 
     st.write("### Train, Validation and Test Set: Labels Frequencies")
 
